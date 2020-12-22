737cbb0800197818e9fd082875d3b993debab6fa
Alexander Nittka <alex@nittka.de>
2020-12-22 09:00:12 +0100

    suppress some more framework linking warnings

--- org.tvtower.bmx/src/org/tvtower/validation/BmxLinkingErrorMessageProvider.java
+++ org.tvtower.bmx/src/org/tvtower/validation/BmxLinkingErrorMessageProvider.java
@@ -16,12 +16,36 @@ public class BmxLinkingErrorMessageProvider extends LinkingDiagnosticMessageProv
 			case "int":
 			case "object":
 			case "string":
+			case "tstringbuilder":
 			case "long":
 			case "float":
 			case "double":
 			case "byte":
 			case "tmap":
+			case "tobjectmap":
+			case "tstringmap":
+			case "tintmap":
+			case "tnodeenumerator":
+			case "tnode":
+			case "tlink":
 			case "tlist":
+			case "tobjectlist":
+			case "short":
+			case "tchannel":
+			case "taudiosample":
+			case "tsound":
+			case "tbank":
+			case "tstream":
+			case "tthread":
+			case "tmutex":
+			case "timage":
+			case "scolor8":
+			case "srect":
+			case "srecti":
+			case "sspriteatlasrect":
+			case "svec2i":
+			case "svec2f":
+			case "tpixmap":
 				return null;
 				//Ignore/Info not yet supported - override org.eclipse.xtext.linking.lazy.LazyLinkingResource.getDiagnosticList(DiagnosticMessage) line 426
 //				severity=Severity.WARNING;
